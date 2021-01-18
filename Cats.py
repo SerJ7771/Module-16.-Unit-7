01a624b37fab5cbd473d944f6e2b7e69c4cc0a85
SerJ7771 <sers7777@gmail.com>
2021-01-18 16:02:33 +0600

    Signed-off-by: SerJ7771 <sers7777@gmail.com>

--- Cats.py
+++ Cats.py
@@ -12,3 +12,13 @@ class Cats:
 
     def getage(self):
         return self.age
+
+from cat import Cats
+
+cat_1 = Cats('Барон', 'мальчик', '2')
+cat_2 = Cats('Сэм', 'мальчик', '2')
+
+pets = [cat_1, cat_2]
+
+for pet in pets:
+    pet.print_list()
